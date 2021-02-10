fc5d8500cadc4c133529e3b37064ffe2c76c9438
MP <princi8@gmail.comm>
2021-02-10 14:33:55 +0100

    v1

--- app.js
+++ app.js
@@ -29,9 +29,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Password Authentication Lab';
 
 const index = require('./routes/index.routes');
 app.use('/', index);
+
+
+
 
 module.exports = app;
